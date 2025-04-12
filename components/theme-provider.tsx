9219c2e37a6c156b254aafd644589eb6e90bdf5b
Vardan <vardhan.boya24@ngit.edu.in>
2025-04-12 14:56:27 +0530

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,5 +1,13 @@
 import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({ children, ...props }: Omit<ThemeProviderProps, 'defaultTheme' | 'forcedTheme'>) {
+  return (
+    <NextThemesProvider 
+      defaultTheme="light" 
+      forcedTheme="light" 
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  )
 }
